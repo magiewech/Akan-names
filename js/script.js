0ad508b5e84df5286c4a04fd00a451c4837c2a97
hawa-maker <hawwawaweru59@gmail.com>
2021-06-23 21:06:27 +0300

    add styles to the initial structure of the webpage

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 console.log("hey yoh")
 var button = document.getElementById("submit")
 button.addEventListener("click",submit)
+radio.addEventListener("checked",female)
+radio.addEventListener("checked",male)
 
 
 
@@ -10,5 +12,9 @@ function submit(event){
     var day = document.getElementById("day").value
     var year = document.getElementById("year").value
     var month = document.getElementById("month").value
-console.log(day,year,month)
+    var female = document.getElementById("female").checked
+    var male = document.getElementById("male").checked
+console.log(day,year,month,female,)
 }
+
+
