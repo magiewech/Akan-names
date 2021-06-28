85708e2c18910fd234d4acb239877bd45656b821
hawa-maker <hawwawaweru59@gmail.com>
2021-06-28 21:08:24 +0300

    add styles to the elements with styles.css

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 console.log("hey yoh")
 var button = document.getElementById("submit")
 button.addEventListener("click",submit)
-radio.addEventListener("checked",female)
-radio.addEventListener("checked",male)
+//
+// radio.addEventListener("checked",female)
+// radio.addEventListener("checked",male)
+
+var femaleAkans = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+var maleAkans =["Kwasi","kwado","Kwabena","Kwaku","Yau","Kofi","Kwame"
+]
 
 
 
@@ -14,7 +19,24 @@ function submit(event){
     var month = document.getElementById("month").value
     var female = document.getElementById("female").checked
     var male = document.getElementById("male").checked
-console.log(day,year,month,female,)
+    var message = "Your Akan name is "
+
+
+    if(female){
+        
+         alert(message + femaleAkans[getWeekDay()])
+    } else if(male){
+        alert(message + maleAkans[getWeekDay()])
+    } else{
+        alert("choose a valid gender")
+    }
+     function getWeekDay()
+   { var birthDay =new Date(year,month,day);
+      console.log(birthDay)
+    var dayOfWeek =birthDay.getDay();
+    return dayOfWeek
+   }
 }
+
 
 
